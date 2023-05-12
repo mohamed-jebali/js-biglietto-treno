40d8c2c94f911595c679bb85ce5257283e397e76
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-12 15:12:44 +0200

    added variables

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let kilometriPercorsi = parseInt(prompt("Quanti kilometri devi percorrere?"));
+let etaPasseggero = parseInt(prompt("Inserisci la tua età"))
+
+
+document.getElementById("output").innerHTML = 1;
+
+console.log(kilometriPercorsi);
+console.log(etaPasseggero);
