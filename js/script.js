f00d36c29547a4ef32d3c982d3141ad8aa810ea1
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-12 15:50:07 +0200

    added conditionals

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 let kilometriPercorsi = parseInt(prompt("Quanti kilometri devi percorrere?"));
 let etaPasseggero = parseInt(prompt("Inserisci la tua età"))
 
-if(kilometriPercorsi == 0.21){
+
+if((kilometriPercorsi * 0.21) + "€"){
+    console.log((kilometriPercorsi * 0.21).toFixed(2))
+}
+else{
+
 }
 
 
-document.getElementById("output").innerHTML = kilometriPercorsi + etaPasseggero ;
+document.getElementById("output").innerHTML = kilometriPercorsi + etaPasseggero + "€";
 
 
-
-console.log(kilometriPercorsi.toFixed(2));
-console.log(etaPasseggero);
