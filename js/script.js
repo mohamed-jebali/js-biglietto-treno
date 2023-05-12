cbb4736c780cea06ff1dd23c16e498d29d483ce6
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-12 15:24:56 +0200

    trying .toFixed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 let kilometriPercorsi = parseInt(prompt("Quanti kilometri devi percorrere?"));
 let etaPasseggero = parseInt(prompt("Inserisci la tua età"))
 
+if(kilometriPercorsi == 0.21){
+}
 
-document.getElementById("output").innerHTML = 1;
 
-console.log(kilometriPercorsi);
+document.getElementById("output").innerHTML = kilometriPercorsi + etaPasseggero ;
+
+
+
+console.log(kilometriPercorsi.toFixed(2));
 console.log(etaPasseggero);
