361b91a3870d6c3ccf978711a48c0336d91a1fc5
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-12 16:28:33 +0200

    working on age number value

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 let kilometriPercorsi = parseInt(prompt("Quanti kilometri devi percorrere?"));
 let etaPasseggero = parseInt(prompt("Inserisci la tua età"))
+let result;
 
 
-if((kilometriPercorsi * 0.21) + "€"){
+
+if((kilometriPercorsi * 0.21) + "€" == result){
     console.log((kilometriPercorsi * 0.21).toFixed(2))
 }
+// else if(isNAN (result)){
+//     alert("Inserisci un numero valido")
+// }
 else{
 
 }
+if (isNaN (etaPasseggero)) {
+    alert("Inserisci un numero valido")
+} else {
+    
+}
 
 
-document.getElementById("output").innerHTML = kilometriPercorsi + etaPasseggero + "€";
+document.getElementById("output").innerHTML = kilometriPercorsi + "€";
 
 
