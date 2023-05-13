d96c61a23caa55695184af7d5bfd78455d19c040
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-13 17:03:36 +0200

    fixed age range discount

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
 let kilometriPercorsi = parseInt(prompt("Quanti kilometri devi percorrere?"));
 let etaPasseggero = parseInt(prompt("Inserisci la tua età"));
-let result = (kilometriPercorsi * 0.21);
+let risultato = (kilometriPercorsi * 0.21);
 
 
-
-
-
-
-if(isNaN(kilometriPercorsi) || isNaN (etaPasseggero)){
+if(kilometriPercorsi === undefined){
+    document.getElementById("output").innerHTML = "Inserisci Qualcosa";
+}
+else if(isNaN(kilometriPercorsi) || isNaN (etaPasseggero)){
     document.getElementById("output").innerHTML = "Inserisci un Numero Valido";
 }
 else{
-    document.getElementById("output").innerHTML = result.toFixed(2) + "€";
+    document.getElementById("output").innerHTML = risultato.toFixed(2) + "€";
 }
 
 
-if(etaPasseggero > 18 && result === (result - ((result / 100) * 20) )){
-    document.getElementById("output").innerHTML = result;
+if(etaPasseggero < 18){
+ let PrezzoScontato = risultato - ((20 / 100) * risultato);
+    document.getElementById("output").innerHTML = PrezzoScontato.toFixed(2);
 }
-else{
-    document.getElementById("output").innerHTML = result + "€";
+else if (etaPasseggero > 65) {
+    let PrezzoScontato = risultato - ((40 / 100) * risultato);
+    document.getElementById("output").innerHTML = PrezzoScontato.toFixed(2);
+}
+ else{
+    document.getElementById("output").innerHTML = risultato.toFixed(2) + "€";
 }
 
-if(etaPasseggero < 65 === (result - ((result / 100) * 40) )){
-    document.getElementById("output").innerHTML = etaPasseggero;
-}
-else{
-    document.getElementById("output").innerHTML = result + "€";
-}
+
 
 
 
