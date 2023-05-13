a453e36f4be76168374fa9a5725a3eca844cb95c
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-13 18:14:55 +0200

    fixed undefined error

--- js/script.js
+++ js/script.js
@@ -3,25 +3,25 @@ let etaPasseggero = parseInt(prompt("Inserisci la tua età"));
 let risultato = (kilometriPercorsi * 0.21);
 
 
-if((kilometriPercorsi) === undefined || (etaPasseggero) === undefined ){
-    document.getElementById("output").innerHTML = "Inserisci Qualcosa";
-}
-else if(isNaN(kilometriPercorsi) || isNaN (etaPasseggero)){
-    document.getElementById("output").innerHTML = "Inserisci un Numero Valido";
-}
 
-
-
-if(etaPasseggero < 18){
- let PrezzoScontato = risultato - ((20 / 100) * risultato);
-    document.getElementById("output").innerHTML = PrezzoScontato.toFixed(2) + "€";
+if(isNaN(kilometriPercorsi) || isNaN (etaPasseggero)){
+    document.getElementById("output").innerHTML = "Inserisci un Numero o Valido";
 }
-else if (etaPasseggero > 65) {
+// else if(undefined (kilometriPercorsi )  || undefined (etaPasseggero)) {
+//     document.getElementById("output").innerHTML = "Inserisci Qualcosa";
+// }
+else{
+if (etaPasseggero < 18){
+    let PrezzoScontato = risultato - ((20 / 100) * risultato);
+       document.getElementById("output").innerHTML = PrezzoScontato.toFixed(2) + "€";
+   }
+   else if (etaPasseggero > 65) {
     let PrezzoScontato = risultato - ((40 / 100) * risultato);
     document.getElementById("output").innerHTML = PrezzoScontato.toFixed(2) + "€";
 }
- else{
+else{
     document.getElementById("output").innerHTML = risultato.toFixed(2) + "€";
+}
 }
 
 
